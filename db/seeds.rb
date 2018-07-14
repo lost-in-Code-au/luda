d16f9b671b7fffcd36bcb4a246b959dffc755313
lost-in-Code-au <reece.c.jones@gmail.com>
2018-07-14 09:17:54 +0100

    faker added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+HOME_GROUP_TITLES - 10.times.map{ Faker::StarWars.call_squadron }
