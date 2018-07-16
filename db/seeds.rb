812e85b381c9684ddaea881b2e7e28a9147bb465
lost-in-Code-au <reece.c.jones@gmail.com>
2018-07-16 07:42:05 +0100

    clean slate with db:schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 HOME_GROUP_TITLES = 10.times.map{ Faker::StarWars.call_squadron }
 
@@ -22,6 +14,6 @@ HOME_GROUPS.each do | home_group |
     students.each do | student |
      newStudent = Student.create(name: student, student_id: Faker::IDNumber.valid, home_group: home_group)   
      newStudent.save
-     puts newStudent.home_group
+    #  puts newStudent.home_group
     end
 end
