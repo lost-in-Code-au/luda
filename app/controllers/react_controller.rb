db9a4a401eb8093540c44b81c66f3cda9f73d3b4
lost-in-Code-au <reece.c.jones@gmail.com>
2018-07-21 18:56:22 +0100

    working react on front end

---
+++ app/controllers/react_controller.rb
@@ -0,0 +1,4 @@
+class ReactController < ApplicationController
+  def root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'app/index'
+  # get 'react/root'
+  root to: "react#root"
 
   devise_for :users
 
